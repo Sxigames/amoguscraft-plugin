8e5166bd4f7deed6c81533ca12e3c1ef02a186c9
Sxigames <sxigames63@gmail.com>
2021-04-06 13:27:51 -0300

    📢 Usar Audiencia para actionbar

--- src/main/java/io/github/sxigames/amogus/Loops/AllLoop.java
+++ src/main/java/io/github/sxigames/amogus/Loops/AllLoop.java
@@ -1,6 +1,7 @@
 package io.github.sxigames.amogus.Loops;
 
 import io.github.sxigames.amogus.Amogus;
+import net.kyori.adventure.text.Component;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -10,16 +11,14 @@ public class AllLoop implements Runnable {
     @Override
     public void run() {
         if(!plugin.getConfig().getBoolean("inPartida"))
-        for(Player p : Bukkit.getOnlinePlayers()){
             if(Bukkit.getOnlinePlayers().size() <= 3) {
-                p.sendActionBar(ChatColor.RED + "" + Bukkit.getOnlinePlayers().size() + " / 10");
+                Bukkit.getServer().sendActionBar(Component.text(ChatColor.RED + "" + Bukkit.getOnlinePlayers().size() + " / 10"));
             }
             if(Bukkit.getOnlinePlayers().size() <= 6 && Bukkit.getOnlinePlayers().size() > 3) {
-                p.sendActionBar(ChatColor.YELLOW + "" + Bukkit.getOnlinePlayers().size() + " / 10");
+                Bukkit.getServer().sendActionBar(Component.text(ChatColor.YELLOW + "" + Bukkit.getOnlinePlayers().size() + " / 10"));
             }
             if(Bukkit.getOnlinePlayers().size() <= 10 && Bukkit.getOnlinePlayers().size() > 7) {
-                p.sendActionBar(Bukkit.getOnlinePlayers().size() + " / 10");
-            }
+                Bukkit.getServer().sendActionBar(Component.text(Bukkit.getOnlinePlayers().size() + " / 10"));
         }
     }
 }
